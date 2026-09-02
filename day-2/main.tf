55668b691e2cfa6f012d25a2784f39affa464de6
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-02 14:19:37 +0530

    main.tf modified

--- day-2/main.tf
+++ day-2/main.tf
@@ -33,10 +33,10 @@ resource "aws_security_group" "sg" {
 
 resource "aws_instance" "server" {
     ami = "ami-01a00762f46d584a1"
-    instance_type = "t3.Micro"
+    instance_type = "t3.micro"
     key_name = "ubuntu"
     vpc_security_group_ids = ["aws_security_group.sg.id"]
-    
+
     user_data = file("/root/Terraform-/day-2/user_data.sh")
 
     tags = {

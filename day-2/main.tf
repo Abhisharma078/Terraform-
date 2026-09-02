2369e017f0b155e97e660723968de671adfe54bb
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-02 14:27:11 +0530

    next

--- day-2/main.tf
+++ day-2/main.tf
@@ -3,8 +3,8 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_security_group" "sg" {
-    name = "security_gr"
-    description = "my_group"
+    name = "security"
+    description = "group"
     vpc_id = data.aws_vpc.default.id
 
     ingress {
@@ -35,7 +35,7 @@ resource "aws_instance" "server" {
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
     key_name = "ubuntu"
-    vpc_security_group_ids = ["aws_security_group.sg.id"]
+    vpc_security_group_ids = [aws_security_group.sg.id]
 
     user_data = file("/root/Terraform-/day-2/user_data.sh")
 

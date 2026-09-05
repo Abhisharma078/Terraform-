bbe73d3604a53f23a97293ed6fdf8e0a577e1c2f
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-05 23:29:02 +0530

    fix: correct user_data assignment to use user_data_base64

--- day-5-module/module/ec2/main.tf
+++ day-5-module/module/ec2/main.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "ec2" {
     key_name = var.key_name
     vpc_security_group_ids = [var.sg]
     subnet_id = var.subnet
-    user_data = filebase64("/root/Terraform-/day-5-module/module/ec2/user_data.sh")
+    user_data_base64 = filebase64("/root/Terraform-/day-5-module/module/ec2/user_data.sh")
     
 }

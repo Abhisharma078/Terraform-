6a61be53989ddc7db66a497de37e658633270470
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-07 00:34:50 +0530

    fix: correct output value references for public and private subnets

--- pratice-module/output.tf
+++ pratice-module/output.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "public_subnet_1a_id" {
-    value =  module.vpc.public_subnet_1a
+    value =  module.vpc.public_subnet_1a_id
 }
 
 output "public_subnet_2b_id" {
-    value =  module.vpc.public_subnet_2b
+    value =  module.vpc.public_subnet_2b_id
 }
 
 output "private_subnet_1a_id" {
-    value =  module.vpc.private_subnet_1a
+    value =  module.vpc.private_subnet_1a_id
 }
 
 output "private_subnet_2b_id" {
-    value =  module.vpc.private_subnet_2b
+    value =  module.vpc.private_subnet_2b_id
 }

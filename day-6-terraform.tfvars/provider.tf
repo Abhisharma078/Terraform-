2be17728bc8f6ac3e5ef557b5d72d03ed38fe162
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-08 01:02:47 +0530

    feat: restructure provider configuration in Terraform file

--- day-6-terraform.tfvars/provider.tf
+++ day-6-terraform.tfvars/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-    region = "ap-south-1"
-    profile = "abhi"
+  region = "ap-south-1"
+  profile = "abhi"
 }

5d016148218fbb507faee141ff8f9f8b09b1b410
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-08 01:54:30 +0530

    fix: correct security group ID reference in EC2 module

--- day-6-terraform.tfvars/main.tf
+++ day-6-terraform.tfvars/main.tf
@@ -22,7 +22,7 @@ module "ec2" {
     ami = var.ami
     key_name = var.key_name
     instance_type = var.instance_type
-    server_sg = module.vpc.sg.sg_id
+    server_sg = module.vpc.sg.id
     public_sb_id = module.vpc.public_subnet_id
     private_sb_id = module.vpc.private_subnet_id
 }

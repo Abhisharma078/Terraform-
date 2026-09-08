277acf7607ac1c7ebc9de89de72ddbd5cc267182
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-08 14:33:13 +0530

    fix: add missing vpc_security_group_ids to public_server instance

--- day-7-state-management/main.tf
+++ day-7-state-management/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "public_server" {
     instance_type = "t3.micro"
     key_name = "ubuntu"
     count = 2
+    vpc_security_group_ids = ["sg-02f87d7e6f3905dc3"]
 
     tags = {
       Name = "public_server"

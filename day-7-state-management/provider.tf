22876faa9f977940f646e50fae367ba5110da6a8
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-08 14:04:18 +0530

    fix: correct typo in shared_credentials_files in provider.tf

--- day-7-state-management/provider.tf
+++ day-7-state-management/provider.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     region = "ap-south-1"
     profile = "abhi"
-    shared_credentails_files = ["/root/.aws/credentials"]
+    shared_credentials_files = ["/root/.aws/credentials"]
     key = "terraform.tfstate"
     use_lockfile = true
   }

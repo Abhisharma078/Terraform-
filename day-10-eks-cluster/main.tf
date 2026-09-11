ec87a14d7c30665fcb073e7c61980248fa85f27d
Abhi Sharma <abhimudgal2k.sharma@gmail.com>
2026-09-12 00:21:49 +0530

    fix: change instance_types to a list format in node group configuration

--- day-10-eks-cluster/main.tf
+++ day-10-eks-cluster/main.tf
@@ -85,7 +85,7 @@ resource "aws_eks_node_group" "nodegroup" {
     node_group_name = "nodegroup"
     node_role_arn = aws_iam_role.node_cluster_role.arn
     subnet_ids = data.aws_subnets.default.ids
-    instance_types = "t3.micro"
+    instance_types = ["t3.micro"]
 
     scaling_config {
       desired_size = 1
